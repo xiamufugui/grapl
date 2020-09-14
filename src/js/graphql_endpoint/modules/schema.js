3006bf23249fb6f549a9b9d2f42d9c7612ef3cc2
andrea-grapl <andrea@graplsecurity.com>
2020-09-13 20:02:19 -0700

    refactor dgraph_client

--- src/js/graphql_endpoint/modules/schema.js
+++ src/js/graphql_endpoint/modules/schema.js
@@ -1,3 +1,5 @@
+const { getDgraphClient } = require('./dgraph_client.js')
+
 const { 
     GraphQLObjectType, 
     GraphQLInt, 
@@ -27,27 +29,7 @@ const { // custom types
     GraplEntityType
 } = require('../modules/API/types.js');
 
-const dgraph = require("dgraph-js");
-const grpc = require("grpc");
 
-const get_random = (list) => {
-    return list[Math.floor((Math.random()*list.length))];
-}
-
-const mg_alpha = get_random(process.env.MG_ALPHAS.split(","));
-
-const getDgraphClient = () => {
-    const clientStub = new dgraph.DgraphClientStub(
-        // addr: optional, default: "localhost:9080"
-        mg_alpha,
-        // credentials: optional, default: grpc.credentials.createInsecure()
-        grpc.credentials.createInsecure(),
-    );
-
-    return new dgraph.DgraphClient(clientStub);
-}
-
-// return lens
 const getLenses = async (dg_client, first, offset) => {
     // console.log("first offset", first, offset);
     const query = `
