8dc9441b9dc5adfe2c9e80b28ac48b3ee828aff9
andrea-grapl <andrea@graplsecurity.com>
2020-11-19 19:32:16 -0700

    pull ajm changes for assigning dGraphClientStub

--- src/js/graphql_endpoint/modules/API/queries/node.js
+++ src/js/graphql_endpoint/modules/API/queries/node.js
@@ -94,26 +94,37 @@ module.exports.getRisksFromNode = async (dg_client, nodeUid) => {
         return
     }
     const query = `
-    query all($a: string)
-    {
-        all(func: uid($a)) @cascade
+        query all($a: string)
         {
-            uid,
-            dgraph_type: dgraph.type
-            node_key
-            risks {
-                uid
+            all(func: uid($a)) @cascade
+            {
+                uid,
                 dgraph_type: dgraph.type
                 node_key
-                analyzer_name
-                risk_score
+                risks {
+                    uid
+                    dgraph_type: dgraph.type
+                    node_key
+                    analyzer_name
+                    risk_score
+                }
             }
         }
-    }`;
+    `;
+
     const txn = dg_client.newTxn();
+    
     try {
         const res = await txn.queryWithVars(query, {'$a': nodeUid});
-        return res.getJson()['all'][0]['risks'];
+        console.log("res", res.getJson())
+        
+        if(res.getJson()[all] === []){
+            return 'no risks'
+            
+        }
+        
+        return res.getJson()['all'][0]['risks']; 
+        
     } finally {
         await txn.discard();
     }
--- src/js/graphql_endpoint/modules/dgraph_client.js
+++ src/js/graphql_endpoint/modules/dgraph_client.js
@@ -7,13 +7,29 @@ const get_random = (list) => {
 
 const mg_alpha = get_random(process.env.MG_ALPHAS.split(","));
 
-module.exports.getDgraphClient = () => {
-    const clientStub = new dgraph.DgraphClientStub(
-        // addr: optional, default: "localhost:9080"
-        mg_alpha,
-        // credentials: optional, default: grpc.credentials.creaSteInsecure()
-        grpc.credentials.createInsecure(),
-    );
+// module.exports.getDgraphClient = () => {
+//     const clientStub = new dgraph.DgraphClientStub(
+//         // addr: optional, default: "localhost:9080"
+//         mg_alpha,
+//         // credentials: optional, default: grpc.credentials.creaSteInsecure()
+//         grpc.credentials.createInsecure(),
+//     );
 
-    return new dgraph.DgraphClient(clientStub);
+//     return new dgraph.DgraphClient(clientStub);
+// }
+let client = null;
+
+module.exports.getDgraphClient = (init_client=false) => {
+    if (init_client || !client) {
+        const clientStub = new dgraph.DgraphClientStub(
+            // addr: optional, default: "localhost:9080"
+            mg_alpha,
+            // credentials: optional, default: grpc.credentials.createInsecure()
+            grpc.credentials.createInsecure(),
+        );
+
+        client = new dgraph.DgraphClient(clientStub)
+    }
+
+    return client;
 }
