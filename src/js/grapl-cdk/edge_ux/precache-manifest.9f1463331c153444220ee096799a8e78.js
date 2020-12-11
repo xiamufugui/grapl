813d804990b446d7fe46af4fdfb529f5c87bda1e
andrea-grapl <andrea@graplsecurity.com>
2020-12-10 18:38:55 -0700

    cdk files

---
+++ src/js/grapl-cdk/edge_ux/precache-manifest.9f1463331c153444220ee096799a8e78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7a9a26d24883de68b5d2339005fe1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd39ec20677210409d0",
+    "url": "/static/css/main.c328d195.chunk.css"
+  },
+  {
+    "revision": "a9cf524a05f493db618d",
+    "url": "/static/js/2.1bb6823c.chunk.js"
+  },
+  {
+    "revision": "ce26cd8b6316c03b9385c0e027e199e6",
+    "url": "/static/js/2.1bb6823c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd39ec20677210409d0",
+    "url": "/static/js/main.9dd50600.chunk.js"
+  },
+  {
+    "revision": "59379d73f145341cdd6f",
+    "url": "/static/js/runtime-main.f4c0327b.js"
+  },
+  {
+    "revision": "803a6c667c909d2f62aac6f8277ef1cd",
+    "url": "/static/media/nodes.803a6c66.jpg"
+  }
+]);
